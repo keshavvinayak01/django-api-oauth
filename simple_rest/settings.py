9788bf03c17fe0cf2ce986210f5fe5965d5674ea
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-08-02 22:18:26 +0530

    Added to templates

--- simple_rest/settings.py
+++ simple_rest/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+
 from global_config import get_global_config
 active_config = get_global_config()
 # Quick-start development settings - unsuitable for production
@@ -58,7 +60,7 @@ ROOT_URLCONF = 'simple_rest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- simple_rest/urls.py
+++ simple_rest/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib.auth import views as auth_views
 from .views import home
+from django.conf import settings
 
 urlpatterns = [
     path('', home, name='home'),
@@ -11,7 +12,7 @@ urlpatterns = [
     path('api-url/', include('rest_framework.urls')),
     path('api/token', TokenObtainPairView.as_view()),
     path('api/token/refresh', TokenRefreshView.as_view()),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_REDIRECT_URL), name='logout'),
     path('auth/', include('social_django.urls', namespace='social'))
 ]
