b9eb8fa8ef154698ebd9ab47829ab5777cb76ead
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-08-03 23:36:12 +0530

    Changed settings for apps, added profile model

--- simple_rest/settings.py
+++ simple_rest/settings.py
@@ -13,12 +13,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 from global_config import get_global_config
 active_config = get_global_config()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -42,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'languages',
-    'social_django'
+    'social_django',
+    'algorithms',
+    'social_app'
 ]
 
 MIDDLEWARE = [
--- social_app/models.py
+++ social_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to=get_user_directory, default = get_default_image)
+    institute = models.CharField(max_length=60)
+    age = models.IntegerField(default=-1)
+    bio = models.TextField(max_length=150)
+
+def get_user_directory()
